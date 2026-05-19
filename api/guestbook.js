4a21b7b7a30d5937d7820e83b090eecaeb334ecc
xtpm <xtpm@users.noreply.github.com>
2026-05-19 06:35:17 -0700

    Use Vercel KV env names

--- api/guestbook.js
+++ api/guestbook.js
@@ -37,8 +37,8 @@ function cleanText(value, fallback, maxLength) {
 }
 
 function getRedisConfig() {
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url = process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL;
+  const token = process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN;
 
   if (!url || !token) {
     return null;
